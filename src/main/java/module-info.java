fb53ba4e6657cd2d83014a3127ddc778e5fbe728
Niranjan B <niranjannb7777@gmail.com>
2022-01-30 17:29:21 +0530

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.txtkm.txtkm {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.txtkm.txtkm to javafx.fxml;
+    exports com.txtkm.txtkm;
+}
